3b796f40bedea39e75313a8d10def25494fb2c9a
João Lucas <163215635+JoaoLucasmcS@users.noreply.github.com>
2024-06-05 18:53:38 -0300

    feat revisao_01

---
+++ Ex_revisao_01/app01.js
@@ -0,0 +1,10 @@
+
+let nome = "John";
+let sobrenome = "Doe";
+let idade = 30;
+const dataNasc = "30/09/1992";
+
+console.log("Nome:", nome);
+console.log("Sobrenome:", sobrenome);
+console.log("Idade:", idade);
+console.log("Data de nascimento:", dataNasc);
